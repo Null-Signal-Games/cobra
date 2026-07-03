2bce8ea3772127a60c36020dc075939fd2771458
Jason Gessner <jason@multiply.org>
2026-07-03 19:12:30 +0000

    Add explicit require for semantic_logger to fix boot dep loading error when deployed.

--- config/puma.rb
+++ config/puma.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require 'semantic_logger'
+
 # This configuration file will be evaluated by Puma. The top-level methods that
 # are invoked here are part of Puma's configuration DSL. For more information
 # about methods provided by the DSL, see https://puma.io/puma/Puma/DSL.html.
