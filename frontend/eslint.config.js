805f7cc33c8481906222a4384c4c95e031e0161e
Ryan Lankin <rlankin@gmail.com>
2026-07-19 12:20:58 -0600

    Miscellaneous minor cleanup

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -1,17 +1,20 @@
-import prettier from 'eslint-config-prettier';
-import path from 'node:path';
-import js from '@eslint/js';
-import svelte from 'eslint-plugin-svelte';
-import { defineConfig, includeIgnoreFile } from 'eslint/config';
-import globals from 'globals';
-import ts from 'typescript-eslint';
+// ts-check
 
-const gitignorePath = path.resolve(import.meta.dirname, '.gitignore');
+import prettier from "eslint-config-prettier";
+import path from "node:path";
+import js from "@eslint/js";
+import svelte from "eslint-plugin-svelte";
+import { defineConfig, includeIgnoreFile } from "eslint/config";
+import globals from "globals";
+import ts from "typescript-eslint";
+
+const gitignorePath = path.resolve(import.meta.dirname, ".gitignore");
 
 export default defineConfig(
 	includeIgnoreFile(gitignorePath),
-	js.configs.recommended,
-	ts.configs.recommended,
+	eslint.configs.recommended,
+	ts.configs.strictTypeChecked,
+  ts.configs.stylisticTypeChecked,
 	svelte.configs.recommended,
 	prettier,
 	svelte.configs.prettier,
@@ -20,22 +23,27 @@ export default defineConfig(
 		rules: {
 			// typescript-eslint strongly recommend that you do not use the no-undef lint rule on TypeScript projects.
 			// see: https://typescript-eslint.io/troubleshooting/faqs/eslint/#i-get-errors-from-the-no-undef-rule-about-global-variables-not-being-defined-even-though-there-are-no-typescript-errors
-			'no-undef': 'off'
+			"no-undef": "off",
+			// "no-console": "error",
+			// "@typescript-eslint/restrict-template-expressions": [
+      //   "error",
+      //   { allowNumber: true },
+      // ],
+      // "@typescript-eslint/no-unused-vars": [
+      //   "error",
+      //   { ignoreRestSiblings: true },
+      // ],
+      // "eol-last": ["error", "always"],
 		}
 	},
 	{
-		files: ['**/*.svelte', '**/*.svelte.ts', '**/*.svelte.js'],
+		files: ["**/*.svelte", "**/*.svelte.ts", "**/*.svelte.js"],
 		languageOptions: {
 			parserOptions: {
 				projectService: true,
-				extraFileExtensions: ['.svelte'],
-				parser: ts.parser
+				extraFileExtensions: [".svelte"],
+				parser: ts.parser,
 			}
 		}
 	},
-	{
-		// Override or add rule settings here, such as:
-		// 'svelte/button-has-type': 'error'
-		rules: {}
-	}
 );
