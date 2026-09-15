236f9505bd61938b15e6505c537cbe2019145c7a
jason gessner <jason@multiply.org>
2026-09-15 04:42:39 +0000

    Add edit stage / table range page and tests.

---
+++ frontend/src/routes/tournaments/[tournamentId]/organizer/edit_stage/[stageId]/+page.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+  import type { PageProps } from "./$types";
+  import StageSettings from "./StageSettings.svelte";
+
+  let { data }: PageProps = $props();
+</script>
+
+<div class="col-12">
+  <StageSettings
+    tournamentId={data.tournamentId}
+    stageId={data.stageId}
+    initialData={data.stageData}
+  />
+</div>
